f0bcdd2fb0380cc2689536e198092f1382bb9ea9
queensk <queenskisivuli@gmail.com>
2023-01-19 16:30:09 +0300

    added execution

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,1 +1,1 @@
-aws cloudformation update-stack --stack-name ourdemoinfra --template-body file://$1    --parameters file://$2  --region=us-east-1
+aws cloudformation update-stack --stack-name $1 --template-body file://$2    --parameters file://$3  --region=us-east-1
